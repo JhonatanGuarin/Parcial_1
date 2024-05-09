c28afcfb0e1f971773bb0947e1a5d59bc28841c1
JhonatanGuarin <jhonatancamilo99@gmail.com>
2024-05-09 18:59:10 -0500

    Avance

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+
+
+
+
+app.set("PORT", process.env.PORT || 3000);
+
+
+app.listen(app.get("PORT"), () =>
+    console.log(`Server listen at Port ${app.get("PORT")}`)
+);
+
+
+// Settings
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+
+// Middlewares
+
+
+
+// Routes
+app.use(require('./routes/index'))
+
+// 404 handler
+
+
+
+// Static
+
+
+
+module.exports = app
---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+const router = require('express').Router()
+const fs = require('fs')
+
+
+const json_data = fs.readFileSync('src/data.json', 'utf-8')
+let data = JSON.parse(json_data)
+console.log(data)
+
+
+
+router.get('/', (req, res) => {
+    res.render('index.ejs', { products: data.products })
+})
+
+module.exports = router
